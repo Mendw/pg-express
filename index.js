f34f82c51aa7b6bec993998ba88130f6c960d343
Jose Mendoza <jomendoza18@gmail.com>
2022-02-18 20:56:48 -0400

    remove static files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import pg from 'pg'
+import cors from 'cors'
+import fileUpload from 'express-fileupload'
+
 import format from 'pg-format'
 
 import 'dotenv/config'
 
 const types = pg.types
-types.setTypeParser(types.builtins.NUMERIC, val => +val)
+const builtinTypes = types.builtins
 
 const app = express()
 const port = 3000
@@ -13,47 +16,65 @@ const port = 3000
 const client = new pg.Client()
 await client.connect()
 
-let reverseTypes = Object.keys(types.builtins).reduce((prev, current) => {
+let reverseTypes = Object.keys(builtinTypes).reduce((prev, current) => {
     return Object.assign(prev, {
-        [types.builtins[current]]: current
+        [builtinTypes[current]]: current
     })
 }, {})
 
-app.use(express.static('public'))
+app.use(cors())
+app.use(fileUpload())
 
-app.get('/data', async (req, res) => {
-    console.log
-    let data = await client.query(`
-    SELECT *
-    FROM pg_catalog.pg_tables
-    WHERE schemaname != 'pg_catalog' AND 
-        schemaname != 'information_schema';
-`)
-
+app.get('/', async (req, res) => {
     res.json({
-        tables: data.rows,
         types: reverseTypes
     })
 })
 
-app.get('/data/:tablename', async (req, res) => {
-    let table = req.params.tablename
-    let data = await client.query(format("SELECT * FROM %I", table))
+app.get('/tables', async (req, res) => {
+    await client.query(`
+    SELECT *
+        FROM pg_catalog.pg_tables
+        WHERE schemaname != 'pg_catalog' AND 
+            schemaname != 'information_schema';
+    `).then(data => {
+        res.json({
+            tables: data.rows
+        })
+    })
+})
 
-    res.json({
-        rows: data.rows,
-        columns: data.fields
+app.get('/tables/:name', async (req, res) => {
+    let table = req.params.name
+
+    await client.query(
+        format("SELECT * FROM %I", table)
+    ).then(data => {
+        res.json({
+            rows: data.rows,
+            columns: data.fields
+        })
+    })
+})
+
+app.post('/parse', async (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let dataFile = req.files.dataFile
+    dataFile.mv(`./data/${dataFile.name}`, (err) => {
+        if(err) return res.status(500).send("oopsy")
+        res.status(200).send('Uplodaded!')
     })
 })
 
 app.listen(port, () => {
-    console.log("listening on " + port)
+    console.log("App started\nListening on port " + port)
 })
 
 async function cleanup() {
-    console.log("\ncleaning up")
     await client.end()
-
     process.exit()
 }
 
